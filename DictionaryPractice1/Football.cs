138e7806ecb875758662dd12355610affcfb7790
rgreene121 <rabib.greene@gmail.com>
2017-08-09 13:38:40 -0400

    Add project files.

---
+++ DictionaryPractice1/Football.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryPractice1
+{
+    class Football
+    {
+        public int TeamId { get; set; }
+        public string TeamName { get; set; }
+        public string Division { get; set; }
+        public string Conference { get; set; }
+
+        public int NetWorth { get; set; }
+
+        public void PrintClub()
+        {
+
+            Console.WriteLine($"The {TeamName} with a team ID of {TeamId} in the {Conference} belong to the {Division} is worth ${NetWorth} ");
+        }
+        public void PrintClub2()
+        {
+
+            Console.WriteLine($"The {TeamName} with a team ID of {TeamId} in the {Conference} belong to the {Division} in the {Conference} are the best ");
+
+
+        }
+    }
+}
---
+++ DictionaryPractice1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryPractice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<int, Football> ballclub = new Dictionary<int, Football>();
+
+            Football fb1 = new Football();
+            fb1.TeamId = 1000;
+            fb1.TeamName = "Browns";
+            fb1.Division = "North";
+            fb1.Conference = "AFC";
+            fb1.NetWorth = 1000000;
+
+            Football fb2 = new Football();
+            fb2.TeamId = 2000;
+            fb2.TeamName = "Colts";
+            fb2.Division = "South";
+            fb2.Conference = "AFC";
+            fb2.NetWorth = 2000000;
+
+            ballclub.Add(fb1.TeamId, fb1);
+            ballclub.Add(fb2.TeamId, fb2);
+
+            foreach (var item in ballclub)
+            {
+                Football kick = item.Value;
+                kick.PrintClub2();
+
+            }
+            Console.ReadLine();
+
+            foreach (KeyValuePair<int, Football> item in ballclub)
+            {
+                Football fb = item.Value;
+                fb.PrintClub();
+
+            }
+
+            Console.ReadLine();
+            
+        }
+    }
+}
